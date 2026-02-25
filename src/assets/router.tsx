fd0922c07b241f3033f4c88bf1ba17d702dabfb4
Elytus <elitinho2502@hotmail.com>
2026-02-24 22:34:20 -0400

    Update

--- src/assets/router.tsx
+++ src/assets/router.tsx
@@ -3,11 +3,18 @@ import { createBrowserRouter } from "react-router";
 //Pages
 import Home from "../pages/Home";
 
+//Documentation
+import Documentation from "../pages/docs/Documentation";
+
 const router = createBrowserRouter(
     [
         {
             path: "/",
-            element: Home(),
+            element: <Home />,
+        },
+        {
+            path: "/documentation",
+            element: <Documentation />,
         }
     ]
 );
